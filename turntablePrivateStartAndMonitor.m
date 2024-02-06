377e71350026fdbfa17d2997ba106accb7aff062
Enzo De Sena <3883487+enzodesena@users.noreply.github.com>
2024-02-06 14:33:00 +0000

    Added gitignore and readme to turntablePrivateStartAndMonitor.

--- turntablePrivateStartAndMonitor.m
+++ turntablePrivateStartAndMonitor.m
@@ -1,3 +1,25 @@
+% turntablePrivateStartAndMonitor Turntable start and monitor function 
+% (private function)
+% 
+% turntablePrivateStartAndMonitor(DIRECTION, PINTOMONITOR, THRESHOLD, 
+% MINIMUMNUMSEC, MAXNUMSEC) Starts rotating the 
+% turntable while monitoring a certain voltage pin on the Arduino. The
+% turntable will stop if the average of two readings on PINTOMONITOR is 
+% above THRESHOLD (in volts). 
+% 
+% DIRECTION can either be 'clockwise' or 'counterclockwise'.
+% PINTOMONITOR is the pin to be monitored on the Arduino (e.g. 'A3')
+% THRESHOLD is the threshold (in volts) above which the turntable will be 
+% stopped.
+% MINIMUMNUMSEC is the duration before which values above the threshold are
+% ignored; this is included such that in case the turntable is already
+% transitioning through an angle it will not stop immediately.
+% MAXNUMSEC is the duration after which the turntable will give up on
+% continuing to rotate.
+%
+%
+% Author: Enzo De Sena
+% Date 3/2/2024
 function successful = turntablePrivateStartAndMonitor(rotationDirection, pinToMonitor, threshold, minimumNumSec, maxNumSec)
     global turntableController;
     if isempty(turntableController)
