a7198947f84611233ff8efe66cd3fbd306d36a22
Enzo De Sena <3883487+enzodesena@users.noreply.github.com>
2024-02-06 14:52:54 +0000

    Fixed the test script.

--- turntableTest.m
+++ turntableTest.m
@@ -11,49 +11,49 @@ function turntableTest()
     
     disp('Rotating back to zero before we can start.')
     
-    controller = turntableToZero(controller, 'clockwise');
+    turntableToZero('clockwise');
     disp('Doing some tests now...')
     
-    assert(turntableGetAngle(controller)==0);
-    controller = turntableTick(controller,'counterclockwise');
-    assert(turntableGetAngle(controller)==357.5);
-    controller = turntableTick(controller,'clockwise');
-    assert(turntableGetAngle(controller)==0);
-    controller = turntableTick(controller,'clockwise');
-    assert(turntableGetAngle(controller)==2.5);
-    controller = turntableTick(controller,'clockwise');
-    controller = turntableTick(controller,'clockwise');
-    assert(turntableGetAngle(controller)==7.5);
+    assert(turntableGetAngle()==0);
+    turntableTick('counterclockwise');
+    assert(turntableGetAngle()==357.5);
+    turntableTick('clockwise');
+    assert(turntableGetAngle()==0);
+    turntableTick('clockwise');
+    assert(turntableGetAngle()==2.5);
+    turntableTick('clockwise');
+    turntableTick('clockwise');
+    assert(turntableGetAngle()==7.5);
     disp('All good so far. Will ask some questions in a second..');
-    controller = turntableToZero(controller, 'counterclockwise');
+    turntableToZero('counterclockwise');
     disp('Is the angle 0 deg? If yes, press enter. If not, please stop as there is something wrong');
     pause
     disp('Good. Let me go to 180 deg now.')
     
     for n=1:72
-        controller = turntableTick(controller, 'clockwise');
-        disp(strcat('Current angle is:', num2str(turntableGetAngle(controller))));
+        turntableTick('clockwise');
+        disp(strcat('Current angle is:', num2str(turntableGetAngle())));
     end
-    assert(turntableGetAngle(controller)==180);
+    assert(turntableGetAngle()==180);
     disp('Is the angle 180 deg? If yes, press enter. If not, please stop as there is something wrong');
     pause
     disp('Good. Let me continue back to 0 deg now.')
     
     
     for n=1:72
-        controller = turntableTick(controller, 'clockwise');
-        disp(strcat('Current angle is:', num2str(turntableGetAngle(controller))));
+        turntableTick('clockwise');
+        disp(strcat('Current angle is:', num2str(turntableGetAngle())));
     end
-    assert(turntableGetAngle(controller)==0);
+    assert(turntableGetAngle()==0);
     disp('Is the angle 0 deg? If yes, press enter. If not, please stop as there is something wrong');
     pause
     disp('Good. Let me go to 0 degrees, this time clockwise')
     
     for n=1:144
-        controller = turntableTick(controller, 'clockwise');
-        disp(strcat('Current angle is:', num2str(turntableGetAngle(controller))));
+        turntableTick('clockwise');
+        disp(strcat('Current angle is:', num2str(turntableGetAngle())));
     end
-    assert(turntableGetAngle(controller)==0);
+    assert(turntableGetAngle()==0);
     disp('Is the angle 0 deg? If yes, press enter. If not, please stop as there is something wrong');
     pause
     disp('Great. Looks like all is working as it should!')
